01c6663235a3ebdf91877fba9dd38c8c937dd6e0
Maurice Monath <81473582+mauke509123@users.noreply.github.com>
2022-02-27 17:27:21 +0100

    1

---
+++ Gameserver/resources/HideMap/client/client.js
@@ -0,0 +1,20 @@
+import * as alt from 'alt';
+import * as native from "natives";
+
+
+
+ alt.setInterval(function(){ CheckMap() }, 1000);
+
+ function CheckMap() {
+    if (!alt.Player.local.vehicle) {
+        native.displayRadar(false);
+        alt.beginScaleformMovieMethodMinimap("SETUP_HEALTH_ARMOUR");
+        native.scaleformMovieMethodAddParamInt(3);
+        native.endScaleformMovieMethod();
+    } else {
+        native.displayRadar(true);
+        alt.beginScaleformMovieMethodMinimap("SETUP_HEALTH_ARMOUR");
+        native.scaleformMovieMethodAddParamInt(3);
+        native.endScaleformMovieMethod();
+    }
+}
---
+++ Gameserver/resources/HideMap/server/server.js
@@ -0,0 +1,1 @@
+console.log("HideMap gestartet")
