9a378b76582058aebb35b3b6fa73844fdc40532e
Maurice Monath <81473582+mauke509123@users.noreply.github.com>
2022-03-11 22:07:44 +0100

    Update server.js
    Co-Authored-By: :/lelek <96193790+Schmax603@users.noreply.github.com>

--- Gameserver/resources/DiscordLog/server/server.js
+++ Gameserver/resources/DiscordLog/server/server.js
@@ -1,4 +1,4 @@
-const token = "CHANGE ME";
+const token = "ODU0MzY0NjU1NjE4NDkwMzk4.YMi3Fw.knfvYEjOyYmc0doQ1W-iTACnRio";
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
